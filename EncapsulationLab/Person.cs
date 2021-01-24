3cc908b79db04c112819e0b6aba15d739e57af1d
mdsisaiful <msisaiful@outlook.com>
2021-01-24 21:12:36 +0100

    Add project files.

---
+++ EncapsulationLab/Person.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationLab
+{
+    class Person
+    {
+        // privata fält
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+
+        // Konstruktor
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        // publika properties
+        public string FirstName
+        {
+
+            get
+            {
+                return this.firstName;
+            }
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+
+            get
+            {
+                return this.lastName;
+            }
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+
+
+        public int Age
+        {
+            get => this.age; // försöker med (=> lambda operator)
+
+
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+
+            get
+            {
+                return this.salary;
+            }
+
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 dollar!");
+                }
+                this.salary = value;
+            }
+        }
+
+        /*
+        // Override ToString metod
+        public override string ToString()
+        {
+            // ToString(): string - override
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+        */
+
+        internal void IncreaseSalary(decimal percentage)
+        {
+            if (age < 30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary + percentage / 200;
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            // ToString(): string - override
+            return $"{firstName} {lastName} receives {salary} dollars.";
+        }
+    }
+}
---
+++ EncapsulationLab/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncapsulationLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             // 1. Sort Persons by Name and Age
+             // ===============================
+             
+            var lines = 5;
+            var persons = new List<Person>(); // skapa en ny lista
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split(); // userinput
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person); // added userinput
+            }
+            persons.OrderBy(p => p.FirstName) // => lambda operator
+            .ThenBy(p => p.Age)
+            .ToList()
+            .ForEach(p => Console.WriteLine(p.ToString())); // ToString metod call
+            */
+
+            // 2. Salary Increase
+            // ==================
+
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                cmdArgs[1],
+                int.Parse(cmdArgs[2]),
+                decimal.Parse(cmdArgs[3]));
+                persons.Add(person);
+            }
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus)); persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
